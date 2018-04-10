1bf5b28f81d6260f87928c5f3e3ee451e9f8dac9
Neethu Joseph <vabsaxena@gmail.com>
2018-04-10 14:30:20 -0500

    added static pages for main page

---
+++ StudioZen/StudioZen/Controllers/MainController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace StudioZen.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: /<controller>/
+		[HttpGet]
+        public IActionResult Index()
+        {
+			
+            return View();
+        }
+		public IActionResult ClassSchedule()
+		{
+			return View();
+		}
+		public IActionResult ClassInfo()
+		{
+			return View();
+		}
+		public IActionResult DisplayInstructors()
+		{
+			return View();
+		}
+			
+	}
+
+}
